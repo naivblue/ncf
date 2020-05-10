7aefa7e54ad4932406f166c1251180e5a0b5238e
ellie.park <ellie.park@kakaocorp.com>
2020-05-11 01:49:20 +0900

    add pro

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,8 @@
 import os
 import json
-
+import collections
+from collections import defaultdict
+import numpy as np
 
 def load_data():
     users = []
@@ -31,4 +33,25 @@ def load_data():
     return users, items, ratings
 
 
+ # item prob for negative sampling
+def item_pro(ratings):
+    items = ratings[1]
+    item_size = len(items)
+    counts = collections.Counter()
+    item_p = np.zeros(item_size)
 
+    for item in items:
+        counts[item] += 1
+    for i in range(item_size):
+        item_p[i] = counts[i]
+    return item_p/np.sum(item_p)
+
+
+# item list per user for negative sampling
+def user_dict(ratings):
+    users = ratings[0]
+    items = ratings[1]
+    user_dict = defaultdict(list)
+    for i in range(len(users)):
+        user_dict[users[i]].append(items[i])
+    return user_dict
